65a8c44fe066caa9498d210ee2c92340bdf6b51b
Anarghya Mitra <amitra93@gmail.com>
2020-12-29 23:29:08 -0600

    etc./.

--- d3LiquidFill.js
+++ d3LiquidFill.js
@@ -35,9 +35,8 @@ function loadLiquidFillGauge(elementId, value, config) {
     if (config == null) config = liquidFillGaugeDefaultSettings();
     console.log("2");
 
-    if (gauge.style("width") == null) return;
-
     var gauge = d3.select("#" + elementId);
+    if (gauge.style("width") == null) return;
     var radius = Math.min(parseInt(gauge.style("width")), parseInt(gauge.style("height"))) / 2;
     var locationX = parseInt(gauge.style("width")) / 2 - radius;
     var locationY = parseInt(gauge.style("height")) / 2 - radius;
