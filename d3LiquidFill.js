7dfecfaa93a040b41e65ce602d8856fb5d128ad6
Anarghya Mitra <amitra93@gmail.com>
2020-12-30 19:50:20 -0600

    etc.

--- d3LiquidFill.js
+++ d3LiquidFill.js
@@ -234,8 +234,7 @@ function loadLiquidFillGauge(elementId, value, config) {
                 font-weight: bold;
             }
             </style>
-			<div
-            id="fillgauge1" width="97%" height="250"></div>
+			<svg xmlns="http://www.w3.org/2000/svg" id="fillgauge1" width="97%" height="250"></svg>
 		`;
 
 	class d3LiquidFill extends HTMLElement {
